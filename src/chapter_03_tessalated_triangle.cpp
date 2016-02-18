51067c81af3c6093e7bc8c6fb1541ff2a87b8e12
Bjorn Tipling <bjorn@ambientchill.com>
2016-02-17 20:18:02 -0800

    Did the tessellated triangle example.

--- src/chapter_03_tessalated_triangle.cpp
+++ src/chapter_03_tessalated_triangle.cpp
@@ -3,14 +3,11 @@
 #include <iostream>
 #include "opengl_tutorials.h"
 
-class chapter_03_animated_triangle : public sb7::application {
+class chapter_03_tessalated_triangle : public sb7::application {
 public:
     void render(double currentTime) {
-        float r = (float) sin(currentTime) * 0.5f + 0.5f;
-        float g = (float) cos(currentTime) * 0.5f + 0.5f;
-        GLfloat red[] = { r, g, 0.0f, 1.0f };
+        GLfloat red[] = { 0.25f, 0.0f, 0.0f, 1.0f };
         glClearBufferfv(GL_COLOR, 0, red);
-        glUseProgram(rendering_program);
 
         GLfloat  attrib[] = {
                 (float)sin(currentTime) * 0.5f,
@@ -18,21 +15,33 @@ public:
                 0.0f, 0.0f
         };
         glVertexAttrib4fv(0, attrib);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glUseProgram(rendering_program);
+        glDrawArrays(GL_PATCHES, 0, 3);
     }
 
     GLuint compile_shaders(void) {
         GLuint vertex_shader;
         GLuint fragment_shader;
+        GLuint control_shader;
+        GLuint eval_shader;
         GLuint program;
 
         std::string vertex_shader_source_string = get_shader("animated_triangle_vertex.vert");
         const GLchar * vertex_shader_source = vertex_shader_source_string.c_str();
 
-        std::string fragment_shader_source_string = get_shader("fragment.vert");
+        std::string fragment_shader_source_string = get_shader("tess_triangle_fragment.vert");
         const GLchar * fragment_shader_source = fragment_shader_source_string.c_str();
 
 
+        std::string control_shader_source_string = get_shader("triangle_control_shader.vert");
+        const GLchar * control_shader_source = control_shader_source_string.c_str();
+
+
+        std::string eval_shader_source_string = get_shader("triangle_eval_shader.vert");
+        const GLchar * eval_shader_source = eval_shader_source_string.c_str();
+
+
+        program = glCreateProgram();
         vertex_shader = glCreateShader(GL_VERTEX_SHADER);
         glShaderSource(vertex_shader, 1, &vertex_shader_source, NULL);
         glCompileShader(vertex_shader);
@@ -41,13 +50,25 @@ public:
         glShaderSource(fragment_shader, 1, &fragment_shader_source, NULL);
         glCompileShader(fragment_shader);
 
-        program = glCreateProgram();
+        control_shader = glCreateShader(GL_TESS_CONTROL_SHADER);
+        glShaderSource(control_shader, 1, &control_shader_source, NULL);
+        glCompileShader(control_shader);
+
+        eval_shader = glCreateShader(GL_TESS_EVALUATION_SHADER);
+        glShaderSource(eval_shader, 1, &eval_shader_source, NULL);
+        glCompileShader(eval_shader);
+
         glAttachShader(program, vertex_shader);
+        glAttachShader(program, control_shader);
+        glAttachShader(program, eval_shader);
         glAttachShader(program, fragment_shader);
-        glLinkProgram(program);
 
         glDeleteShader(vertex_shader);
+        glDeleteShader(control_shader);
+        glDeleteShader(eval_shader);
         glDeleteShader(fragment_shader);
+
+        glLinkProgram(program);
 
         return program;
     }
@@ -56,6 +77,7 @@ public:
         rendering_program = compile_shaders();
         glCreateVertexArrays(1, &vertex_array_object);
         glBindVertexArray(vertex_array_object);
+        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
     }
 
     void shutdown() {
@@ -70,4 +92,4 @@ private:
 
 };
 
-DECLARE_MAIN(chapter_03_animated_triangle);
+DECLARE_MAIN(chapter_03_tessalated_triangle);
